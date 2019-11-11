f86fe8498554cdcf36bf58931fbab3cc0f19c0cc
wws11 <17637900215@163.com>
2019-11-11 10:39:15 +0800

    增加一个排序操作

---
+++ src/main/java/com/soecode/lyf/demo/test/collection/SortTest.java
@@ -0,0 +1,15 @@
+package com.soecode.lyf.demo.test.collection;
+
+/**
+ * 下面是一个集合排序的操作按照中文首字母排序
+ *
+ * @author 魏文思
+ * @date 2019/11/11$ 10:35$
+ */
+public class SortTest {
+
+       /* eduWorkerInfos.sort((o1, o2) -> {
+        Collator instance = Collator.getInstance(Locale.CHINA);
+        return instance.compare(o1.getRealName(), o2.getRealName());
+    });*/
+}
